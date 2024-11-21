6b7f4f81087921daae2721561379df7721234f96
nadlabka <ipavlovsky3@gmail.com>
2024-11-21 23:16:35 +0100

    started AssetsManager

--- src/Utils/DataStructures/SolidVector.h
+++ src/Utils/DataStructures/SolidVector.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <vector>
+#include <cassert>
+#include <DebugMacros.h>
 
 template <typename T>
 class SolidVector
@@ -8,19 +10,28 @@ public:
     using ID = uint32_t;
     using Index = uint32_t;
 
-    Index size() const { return Index(m_data.size()); }
+    uint32_t size() const { return uint32_t(m_data.size()); }
 
     const T* data() const { return m_data.data(); }
     T* data() { return m_data.data(); }
 
-    const T& operator[](ID id) const { return m_data[m_forwardMap[id]]; }
-    T& operator[](ID id) { return m_data[m_forwardMap[id]]; }
+    const T& operator[](ID id) const 
+    {
+        ASSERT(isValidID(id), "Invalid ID");
+        return m_data[m_forwardMap[id]];
+    }
+    T& operator[](ID id) 
+    {
+        ASSERT(isValidID(id), "Invalid ID");
+        return m_data[m_forwardMap[id]];
+    }
 
-    ID insert(const T& value)
+    template <typename... Args>
+    ID insert(Args&&... args) 
     {
         ID id = m_nextUnused;
 
-        if (id == m_forwardMap.size())
+        if (id == m_forwardMap.size()) 
         {
             m_forwardMap.push_back(Index(m_forwardMap.size() + 1));
         }
@@ -28,31 +39,34 @@ public:
         m_nextUnused = m_forwardMap[id];
         m_forwardMap[id] = Index(m_data.size());
 
-        m_data.emplace_back(value);
+        m_data.emplace_back(std::forward<Args>(args)...);
         m_backwardMap.emplace_back(id);
 
         return id;
     }
 
-    void erase(ID id)
+    void erase(ID id) 
     {
+        ASSERT(isValidID(id), "Invalid ID for erase operation");
+
         Index& forwardIndex = m_forwardMap[id];
 
+        // Move the last element to the erased position
         m_data[forwardIndex] = std::move(m_data.back());
         m_data.pop_back();
 
         ID backwardIndex = m_backwardMap.back();
-
         m_backwardMap[forwardIndex] = backwardIndex;
         m_backwardMap.pop_back();
 
         m_forwardMap[backwardIndex] = forwardIndex;
 
+        // Mark the ID as unused
         forwardIndex = m_nextUnused;
         m_nextUnused = id;
     }
 
-    void clear()
+    void clear() 
     {
         m_forwardMap.clear();
         m_backwardMap.clear();
@@ -60,7 +74,7 @@ public:
         m_nextUnused = 0;
     }
 
-    void reserve(Index count)
+    void reserve(Index count) 
     {
         m_data.reserve(count);
         m_forwardMap.reserve(count);
@@ -68,9 +82,13 @@ public:
     }
 
 protected:
+    bool isValidID(ID id) const 
+    {
+        return id < m_forwardMap.size() && m_forwardMap[id] < m_data.size();
+    }
+
     std::vector<T> m_data;
     std::vector<Index> m_forwardMap;
     std::vector<ID> m_backwardMap;
-
     ID m_nextUnused = 0;
 };
