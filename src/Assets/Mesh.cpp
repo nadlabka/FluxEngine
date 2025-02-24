8614f78916d8a5310ff766560e4dece57794014e
nadlabka <ipavlovsky3@gmail.com>
2025-02-24 14:03:57 +0100

    PerMesh Instance Data managing functions for StaticMesh

--- src/Assets/Mesh.cpp
+++ src/Assets/Mesh.cpp
@@ -1,7 +1,53 @@
 #include "stdafx.h"
 #include "Mesh.h"
 
-void Assets::StaticMesh::AddPerInstanceData(entt::entity ent, const MeshPerInstanceData& data)
+void Assets::StaticMesh::SetPerInstanceData(Core::Entity ent, const MeshPerInstanceData& data)
 {
-	asasas
+	ASSERT(m_meshPerInstanceDataBuffers.dataBuffer && m_meshPerInstanceDataBuffers.uploadBuffer, "You have to set RHI buffer before adding any PerInstance data");
+	if (!m_meshPerInstanceData.contains(ent))
+	{
+		m_meshPerInstanceData.emplace(ent);
+		m_meshPerInstanceDataBuffers.dirtyIndices.push_back(m_meshPerInstanceData.size());
+	}
+	else
+	{
+		m_meshPerInstanceDataBuffers.dirtyIndices.push_back(m_meshPerInstanceData.index(ent));
+	}
+
+	m_meshPerInstanceData.patch(ent, data);
+
+	size_t perInstanceDataPoolIndex = m_meshPerInstanceData.index(ent);
+	for (auto& submesh : m_submeshes)
+	{
+		submesh.SetPerInstanceData<MeshPerInstanceDataHandle>(ent, perInstanceDataPoolIndex);
+	}
+}
+
+void Assets::StaticMesh::SetRHIBuffersForPerInstanceData(std::shared_ptr<RHI::IBuffer> uploadBuffer, std::shared_ptr<RHI::IBuffer> dataBuffer)
+{
+	if (!m_meshPerInstanceDataBuffers.dataBuffer || !m_meshPerInstanceDataBuffers.uploadBuffer)
+	{
+		m_meshPerInstanceDataBuffers = BuffersWithDirtyIndices{ uploadBuffer, dataBuffer, {} };
+	}
+	else
+	{
+		m_meshPerInstanceDataBuffers.uploadBuffer = uploadBuffer;
+		m_meshPerInstanceDataBuffers.dataBuffer = dataBuffer;
+	}
+}
+
+void Assets::StaticMesh::UpdateRHIBufferWithPerInstanceData(std::shared_ptr<RHI::ICommandBuffer> commandBuffer)
+{
+	for (auto& dirtyIndex : m_meshPerInstanceDataBuffers.dirtyIndices)
+	{
+		RHI::BufferRegionCopyDescription regionDesc =
+		{
+			.srcOffset = sizeof(MeshPerInstanceData) * dirtyIndex,
+			.destOffset = sizeof(MeshPerInstanceData) * dirtyIndex,
+			.width = sizeof(MeshPerInstanceData)
+		};
+		m_meshPerInstanceDataBuffers.uploadBuffer->UploadData((void*)*m_meshPerInstanceData.raw(), regionDesc); // maybe upload as contiguous data to improve cache hits on gpu when copying 1-by-1
+		commandBuffer->CopyDataBetweenBuffers(m_meshPerInstanceDataBuffers.uploadBuffer, m_meshPerInstanceDataBuffers.dataBuffer, regionDesc);
+	}
+	m_meshPerInstanceDataBuffers.dirtyIndices.clear();
 }
--- src/Assets/Mesh.h
+++ src/Assets/Mesh.h
@@ -11,7 +11,9 @@ namespace Assets
 
 	struct StaticMesh
 	{
-		void AddPerInstanceData(entt::entity ent, const MeshPerInstanceData& data);
+		void SetPerInstanceData(Core::Entity ent, const MeshPerInstanceData& data);
+		void SetRHIBuffersForPerInstanceData(std::shared_ptr<RHI::IBuffer> uploadBuffer, std::shared_ptr<RHI::IBuffer> dataBuffer);
+		void UpdateRHIBufferWithPerInstanceData(std::shared_ptr<RHI::ICommandBuffer> commandBuffer);
 
 		std::vector<StaticSubmesh> m_submeshes;
 		entt::storage<MeshPerInstanceData> m_meshPerInstanceData;
