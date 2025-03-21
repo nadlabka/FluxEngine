241a5089e09bdb06d06c2e5b012f73a4c8a52820
nadlabka <ipavlovsky3@gmail.com>
2025-03-21 15:56:46 +0100

    Submehs instances with correct PerMesh data references and buffer resizes part1

--- src/Assets/Submesh.cpp
+++ src/Assets/Submesh.cpp
@@ -5,3 +5,57 @@ Assets::StaticSubmesh::StaticSubmesh(RHI::BufferWithRegionDescription primaryVer
 	: m_primaryVertexData(primaryVertexData), m_secondaryVertexData(secondaryVertexData), m_indicesData(indicesData), m_registry()
 {
 }
+
+void Assets::PrivateUploadBuffersPair::Resize(uint32_t newElementCount, uint32_t elementStride, std::shared_ptr<RHI::ICommandBuffer> commandBuffer)
+{
+    auto& rhiContext = RHI::RHIContext::GetInstance();
+    auto allocator = rhiContext.GetAllocator();
+
+    if (!dataBuffer || !uploadBuffer) 
+    {
+        RHI::BufferDescription desc{};
+        desc.elementsNum = newElementCount;
+        desc.elementStride = elementStride;
+        desc.flags = { .requiredCopyStateToInit = true };
+        desc.usage = RHI::BufferUsage::DataReadOnlyBuffer;
+
+        desc.access = RHI::BufferAccess::Upload;
+        desc.usage = RHI::BufferUsage::None;
+        uploadBuffer = allocator->CreateBuffer(desc);
+
+        desc.access = RHI::BufferAccess::DefaultPrivate;
+        desc.usage = RHI::BufferUsage::DataReadOnlyBuffer;
+        dataBuffer = allocator->CreateBuffer(desc);
+        return;
+    }
+
+    RHI::BufferDescription desc{};
+    desc.elementsNum = newElementCount;
+    desc.elementStride = elementStride;
+    desc.flags = { .requiredCopyStateToInit = true };
+    desc.usage = RHI::BufferUsage::DataReadOnlyBuffer;
+
+    desc.access = RHI::BufferAccess::Upload;
+    desc.usage = RHI::BufferUsage::None;
+    auto newUploadBuffer = allocator->CreateBuffer(desc);
+
+    desc.access = RHI::BufferAccess::DefaultPrivate;
+    desc.usage = RHI::BufferUsage::DataReadOnlyBuffer;
+    auto newDataBuffer = allocator->CreateBuffer(desc);
+
+
+    uint32_t oldElementCount = dataBuffer->GetStructuredElementsNum();
+
+    RHI::BufferRegionCopyDescription copyDesc
+    {
+        .srcOffset = 0,
+        .destOffset = 0,
+        .width = oldElementCount * elementStride
+    };
+
+    commandBuffer->CopyDataBetweenBuffers(dataBuffer, newDataBuffer, copyDesc);
+    commandBuffer->CopyDataBetweenBuffers(uploadBuffer, newUploadBuffer, copyDesc);
+    
+    dataBuffer = std::move(newDataBuffer);
+    uploadBuffer = std::move(newUploadBuffer);
+}
--- src/Assets/Submesh.h
+++ src/Assets/Submesh.h
@@ -14,11 +14,25 @@ namespace Assets
 		uint32_t indexInPool;
 	};
 
-	struct BuffersWithDirtyIndices
+	struct PrivateUploadBuffersPair
 	{
 		std::shared_ptr<RHI::IBuffer> uploadBuffer;
 		std::shared_ptr<RHI::IBuffer> dataBuffer;
+
+		void Resize(uint32_t newElementCount, uint32_t elementStride, std::shared_ptr<RHI::ICommandBuffer> commandBuffer);
+	};
+
+	struct BuffersWithDirtyIndices : PrivateUploadBuffersPair
+	{
 		std::vector<uint32_t> dirtyIndices;
+	};
+
+	struct PerMaterialContextData
+	{
+		BuffersWithDirtyIndices materialParamsBuffers;
+
+		entt::storage<MeshPerInstanceDataHandle> perMeshData;
+		PrivateUploadBuffersPair perMeshBuffers;
 	};
 
 	class StaticSubmesh
@@ -28,66 +42,68 @@ namespace Assets
 		StaticSubmesh(RHI::BufferWithRegionDescription primaryVertexData, RHI::BufferWithRegionDescription secondaryVertexData, RHI::BufferWithRegionDescription indicesData);
 
 		template <typename T, typename... Args>
-		void SetPerInstanceData(Core::Entity entity, Args&&... args) // we need to delete all requested instances first and insert only after this
+		void CreatePerInstanceData(Core::Entity entity, const MeshPerInstanceDataHandle& perMeshHandle, Args&&... args) // we need to delete all requested instances first and insert only after this
 		{
-			auto* bufferWithIndices = m_registry.ctx().find<BuffersWithDirtyIndices>(entt::type_id<T>().hash());
-			ASSERT(bufferWithIndices, "You have to set RHI buffer before adding any PerInstance data");
+			auto* perMaterialContextData = m_registry.ctx().find<PerMaterialContextData>(entt::type_id<T>().hash());
+			ASSERT(perMaterialContextData, "You have to set RHI buffers before adding any PerInstance data");
 
-			if (!m_registry.valid(entity))
-			{
-				m_registry.create(entity);
-				bufferWithIndices->dirtyIndices.push_back(m_registry.storage<T>().size());
-			}
-			else
-			{
-				bufferWithIndices->dirtyIndices.push_back(m_registry.storage<T>().index(entity));
-			}
+			ASSERT(!m_registry.valid(entity), "Instance for this entity has already been created");
 
-			m_registry.emplace_or_replace<T>(entity, std::forward<Args>(args)...);
-		} 
+			m_registry.create(entity);
+			perMaterialContextData->materialParamsBuffers.dirtyIndices.push_back(m_registry.storage<T>().size());
 
-		void DestroyEntityReference(Core::Entity entity) // we need to delete all requested instances first and insert only after this
+			perMaterialContextData->perMeshData.emplace(entity, perMeshHandle);
+
+			m_registry.emplace<T>(entity, std::forward<Args>(args)...);
+		}
+
+		template <typename T, typename... Args>
+		void UpdatePerInstancePerMaterialData(Core::Entity entity, Args&&... args)
+		{
+			ASSERT(m_registry.valid(entity) && m_registry.any_of<T>(entity), "PerInstance has to be set first before being updated");
+			m_registry.replace<T>(entity, args);
+		}
+
+		template <typename T>
+		void UpdatePerInstancePerMeshData(Core::Entity entity, const MeshPerInstanceDataHandle& perMeshHandle)
+		{
+			ASSERT(m_registry.valid(entity) && m_registry.ctx().contains(entt::type_id<T>().hash()), "PerInstance has to be set first before being updated");
+			auto* perMaterialContextData = m_registry.ctx().find<PerMaterialContextData>(entt::type_id<T>().hash());
+			perMaterialContextData->perMeshData.replace(entity, perMeshHandle);
+		}
+
+		void DestroyEntityReference(Core::Entity entity, Core::Entity swappedMesh, const MeshPerInstanceDataHandle& perMeshHandle) // we need to delete all requested instances first and insert only after this
 		{
 			for (auto&& typedStorage : m_registry.storage())
 			{
 				auto storageType = typedStorage.first;
 				auto& storage = typedStorage.second;
 
-				auto* bufferWithIndices = m_registry.ctx().find<BuffersWithDirtyIndices>(storageType);
-				bufferWithIndices->dirtyIndices.push_back(storage.index(entity));
+				auto* perMaterialContextData = m_registry.ctx().find<PerMaterialContextData>(storageType);
+
+				if (!storage.contains(swappedMesh))
+				{
+					perMaterialContextData->perMeshData.get(swappedMesh) = perMeshHandle;
+				}
+
+				if (storage.contains(entity))
+				{
+					size_t indexInPool = storage.index(entity);
+
+					perMaterialContextData->materialParamsBuffers.dirtyIndices.push_back(indexInPool);
+
+					perMaterialContextData->perMeshData.remove(entity);
+				}
 			}
 			m_registry.destroy(entity);
 		}
-		
-		template<typename T>
-		void SetRHIBuffersForPerInstanceData(std::shared_ptr<RHI::IBuffer> uploadBuffer, std::shared_ptr<RHI::IBuffer> dataBuffer)
-		{
-			auto* bufferWithIndices = m_registry.ctx().find<BuffersWithDirtyIndices>(entt::type_id<T>().hash());
-			if (!bufferWithIndices)
-			{
-				m_registry.ctx().insert_or_assign(entt::type_id<T>().hash(), BuffersWithDirtyIndices{ uploadBuffer, dataBuffer, {} });
-			}
-			else
-			{
-				bufferWithIndices->uploadBuffer = uploadBuffer;
-				bufferWithIndices->dataBuffer = dataBuffer;
-			}
-		}
 
 		template<typename T>
-		std::shared_ptr<RHI::IBuffer> GetRHIBufferForPerInstanceData() const
-		{
-			auto* bufferWithIndices = m_registry.ctx().find<BuffersWithDirtyIndices>(entt::type_id<T>().hash());
-			ASSERT(bufferWithIndices, "No buffer found for the given type");
-			return bufferWithIndices->dataBuffer;
-		}
-
-		template<typename T>
-		void UpdateRHIBufferWithPerInstanceData(std::shared_ptr<RHI::ICommandBuffer> commandBuffer)
+		void UpdateRHIBuffersWithPerInstanceData(std::shared_ptr<RHI::ICommandBuffer> commandBuffer)
 		{
 			auto& storage = m_registry.storage<T>();
-			auto* bufferWithIndices = m_registry.ctx().find<BuffersWithDirtyIndices>(entt::type_id<T>().hash());
-			for (auto& dirtyIndex : bufferWithIndices->dirtyIndices)
+			auto* perMaterialContextData = m_registry.ctx().find<PerMaterialContextData>(entt::type_id<T>().hash());
+			for (auto& dirtyIndex : perMaterialContextData->materialParamsBuffers.dirtyIndices)
 			{
 				RHI::BufferRegionCopyDescription regionDesc =
 				{
@@ -95,13 +111,38 @@ namespace Assets
 					.destOffset = sizeof(T) * dirtyIndex,
 					.width = sizeof(T)
 				};
-				bufferWithIndices->uploadBuffer->UploadData((void*)*storage.raw(), regionDesc); // maybe upload as contiguous data to improve cache hits on gpu when copying 1-by-1
-				commandBuffer->CopyDataBetweenBuffers(bufferWithIndices->uploadBuffer, bufferWithIndices->dataBuffer, regionDesc);
+				perMaterialContextData->materialParamsBuffers.uploadBuffer->UploadData((void*)*storage.raw(), regionDesc); // maybe upload as contiguous data to improve cache hits on gpu when copying 1-by-1
+				commandBuffer->CopyDataBetweenBuffers(perMaterialContextData->materialParamsBuffers.uploadBuffer, perMaterialContextData->materialParamsBuffers.dataBuffer, regionDesc);
+
+				regionDesc =
+				{
+					.srcOffset = sizeof(MeshPerInstanceDataHandle) * dirtyIndex,
+					.destOffset = sizeof(MeshPerInstanceDataHandle) * dirtyIndex,
+					.width = sizeof(MeshPerInstanceDataHandle)
+				};
+				perMaterialContextData->perMeshBuffers.uploadBuffer->UploadData((void*)*(perMaterialContextData->perMeshData.raw(), regionDesc); // maybe upload as contiguous data to improve cache hits on gpu when copying 1-by-1
+				commandBuffer->CopyDataBetweenBuffers(perMaterialContextData->perMeshBuffers.uploadBuffer, perMaterialContextData->perMeshBuffers.dataBuffer, regionDesc);
 			}
-			bufferWithIndices->dirtyIndices.clear();
+			perMaterialContextData->materialParamsBuffers.dirtyIndices.clear();
 		}
 
-		template<typename T>
+		template <typename T>
+		std::shared_ptr<RHI::IBuffer> GetRHIBufferForPerMaterialData() const
+		{
+			auto* perMaterialContextData = m_registry.ctx().find<PerMaterialContextData>(entt::type_id<T>().hash());
+			ASSERT(perMaterialContextData, "No per-material context found");
+			return perMaterialContextData->materialParamsBuffers.dataBuffer;
+		}
+
+		template <typename T>
+		std::shared_ptr<RHI::IBuffer> GetRHIBufferForPerMeshData() const
+		{
+			auto* perMaterialContextData = m_registry.ctx().find<PerMaterialContextData>(entt::type_id<T>().hash());
+			ASSERT(perMaterialContextData, "No per-material context found");
+			return perMaterialContextData->perMeshBuffers.dataBuffer;
+		}
+
+		template <typename T>
 		uint32_t GetActiveInstancesNum()
 		{
 			return m_registry.storage<T>().size();
@@ -123,6 +164,37 @@ namespace Assets
 		}
 
 	private:
+		template <typename T>
+		void InitializeBuffers(PerMaterialContextData& contextData, std::shared_ptr<RHI::ICommandBuffer> commandBuffer)
+		{
+			auto& rhiContext = RHI::RHIContext::GetInstance();
+			auto allocator = rhiContext.GetAllocator();
+
+			contextData.materialParamsBuffers.Resize(16, sizeof(T), allocator, commandBuffer);
+			contextData.perMeshBuffers.Resize(16, sizeof(MeshPerInstanceDataHandle), allocator, commandBuffer);
+		}
+
+		template <typename T>
+		void EnsureBufferCapacity(PerMaterialContextData& contextData, uint32_t requiredSize, std::shared_ptr<RHI::ICommandBuffer> commandBuffer)
+		{
+			auto& rhiContext = RHI::RHIContext::GetInstance();
+			auto allocator = rhiContext.GetAllocator();
+
+			auto& materialBuffer = contextData.materialParamsBuffers.dataBuffer;
+			uint32_t currentMaterialSize = materialBuffer ? materialBuffer->GetStructuredElementsNum() : 0;
+			if (currentMaterialSize < requiredSize)
+			{
+				contextData.materialParamsBuffers.Resize(requiredSize * 2, sizeof(T), allocator, commandBuffer);
+			}
+
+			auto& perMeshBuffer = contextData.perMeshBuffers.dataBuffer;
+			uint32_t currentPerMeshSize = perMeshBuffer ? materialBuffer->GetStructuredElementsNum() : 0;
+			if (currentPerMeshSize < requiredSize)
+			{
+				contextData.perMeshBuffers.Resize(requiredSize * 2, sizeof(MeshPerInstanceDataHandle), allocator, commandBuffer);
+			}
+		}
+
 		RHI::BufferWithRegionDescription m_primaryVertexData;
 		RHI::BufferWithRegionDescription m_secondaryVertexData;
 		RHI::BufferWithRegionDescription m_indicesData;
